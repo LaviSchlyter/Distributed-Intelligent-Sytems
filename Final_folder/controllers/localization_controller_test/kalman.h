8b0efa0813b397e01ad08e8f318869d8fa3ff4c3
schlyter <lavinia.schlyter@epfl.ch>
2021-06-01 10:32:32 +0200

    Deleted test controller for localization

--- Final_folder/controllers/localization_controller_test/kalman.h
+++
@@ -1,9 +0,0 @@
-#ifndef KALMAN_H
-#define KALMAN_H
-
-#include "utils.h"
-
-/// Documentation in c file
-void compute_kalman_acc(pose_t* pos_kal_acc, const int time_step, double time_now, const double heading, const measurement_t meas_, const pose_t pose_);
-void compute_kalman_wheels(pose_t* pos_kal_wheel, const int time_step, double time_now, double Aleft_enc, double Aright_enc,const pose_t pose_);
-#endif
