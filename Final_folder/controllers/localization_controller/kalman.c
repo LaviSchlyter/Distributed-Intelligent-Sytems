f24665c7afa996c795abdc28d6590f7ffa23440f
schlyter <lavinia.schlyter@epfl.ch>
2021-06-05 18:46:53 +0200

    Removed redefination of time step

--- Final_folder/controllers/localization_controller/kalman.c
+++ Final_folder/controllers/localization_controller/kalman.c
@@ -20,7 +20,7 @@
 #define VERBOSE_ACC_KAL false                       // Print the accelerometer Kalman
 #define VERBOSE_WHEEL_KAL true                       // Print the wheel encoder Kalman
 
-int time_step;
+
 /// Variables to store the pose of wheel and the position+velocities for accelerometer
 static double X_wheel[3][1], X_acc[4][1];
 static double X_new_wheel[3][1], X_new_acc[4][1];
